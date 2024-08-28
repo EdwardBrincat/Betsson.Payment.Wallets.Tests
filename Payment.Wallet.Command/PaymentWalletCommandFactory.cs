867848e43d320fd747fa2404130de52aa361bcc9
EdwardBrincat <umm360@gmail.com>
2024-08-28 08:45:46 +0200

    Added Command Factory

---
+++ Payment.Wallet.Command/PaymentWalletCommandFactory.cs
@@ -0,0 +1,25 @@
+﻿using Payment.Wallet.Client.Model;
+using Payment.Wallet.Services;
+
+namespace Payment.Wallet.Command;
+
+public class PaymentWalletCommandFactory
+{
+    private readonly IPaymentWalletService _service;
+
+    public PaymentWalletCommandFactory(
+        IPaymentWalletService service
+    )
+    {
+        _service = service;
+    }
+
+    public Task<PaymentWalletResponse> ExecuteGetWalletBalanceCommand()
+        => _service.GetWalletBalance();
+
+    public Task<PaymentWalletResponse> ExecuteDepositCommand(PaymentWalletRequest paymentWalletRequest)
+        => _service.Deposit(paymentWalletRequest);
+
+    public Task<PaymentWalletResponse> ExecuteWithdrawCommand(PaymentWalletRequest paymentWalletRequest)
+        => _service.Withdraw(paymentWalletRequest);
+}
